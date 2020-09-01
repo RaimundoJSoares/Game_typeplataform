4be3de4d5dff3b1dd0f43da1a99ffe620e022fba
KazumaShachou <raianimes1000@gmail.com>
2020-09-01 05:09:37 -0400

    Implementation of Jump

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -7,6 +7,10 @@ public class PlayerJump : MonoBehaviour
     public Rigidbody2D rb;
 
     public float JumpForce = 50f;
+
+    bool inGround;
+
+    public float distance = 1;
     void Start()
     {
 
@@ -15,9 +19,19 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && inGround)
         {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(Vector2.up * JumpForce);
         }
+    }
+    private void FixedUpdate()
+    {
+        inGround = Physics2D.Raycast(transform.position, Vector2.down, distance, 1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawRay(transform.position, Vector2.down * distance, inGround ? Color.blue : Color.red);
+        // ? representa se, e o : se não
+
+        //transfrm position é a posição do jogador  , vetor de posição para baixo vezes a distance, e tambem a layer mask
+
     }
 }
