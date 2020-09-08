ec7af2048cea336bdef04259e284b1e7ec628ca7
KazumaShachou <raianimes1000@gmail.com>
2020-09-08 12:57:20 -0400

    Mace enemy

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Velocity = 1f;
+
+    public Vector2 direction = Vector2.right;
+
+    public bool reversedirection;
+
+    public float inverttime = 1f;
+
+    int newdirection = 1;
+
+    private void Start()
+    {
+        if (reversedirection)
+        {
+
+            InvokeRepeating("Reversedirection", inverttime, inverttime);
+        }
+
+    }
+    void Reversedirection()
+    {
+        newdirection *= -1;
+
+    }
+
+
+    private void Update()
+
+    {
+
+        transform.Translate(direction * Velocity * newdirection * Time.deltaTime);
+    }
+}
