581d0183346feb64046d258f61a50996938600f8
KazumaShachou <raianimes1000@gmail.com>
2020-09-08 13:52:06 -0400

    Death

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+
+    public float ydeath;
+    private void Update()
+    {
+        if (transform.position.y < ydeath)
+        {
+            Die();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
